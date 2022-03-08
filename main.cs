a3aa68b798fabfbe0ef4c9bd1a55804f01437754
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-08 18:36:55 +0000

    Removed extra spaces.

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@
 
 using System;
 
-class Program 
+class Program
 {
-    public static void Main (string[] args) 
+    public static void Main (string[] args)
     {
         Console.WriteLine ("Hello World");
         Console.WriteLine("\nFinished.");
