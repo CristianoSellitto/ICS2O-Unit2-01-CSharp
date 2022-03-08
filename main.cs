0a364fc092ea39af6eccee67c9264c2bc14f1531
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-08 18:25:25 +0000

    Added finished text

--- main.cs
+++ main.cs
@@ -1,7 +1,10 @@
+// Cristiano | ICS2O-Unit2-01-CSharp | March 8 2022
+
 using System;
 
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+    Console.WriteLine("\nFinished.");
   }
 }
