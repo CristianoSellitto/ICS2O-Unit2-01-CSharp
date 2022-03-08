35f4b80a59874561bccb1ab553dab76a257f08f6
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-08 18:33:20 +0000

    4 spaced the code.

--- main.cs
+++ main.cs
@@ -4,9 +4,9 @@ using System;
 
 class Program 
 {
-  public static void Main (string[] args) 
-  {
-    Console.WriteLine ("Hello World");
-    Console.WriteLine("\nFinished.");
-  }
+    public static void Main (string[] args) 
+    {
+        Console.WriteLine ("Hello World");
+        Console.WriteLine("\nFinished.");
+    }
 }
